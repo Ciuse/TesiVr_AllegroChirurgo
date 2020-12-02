f2f1f79a7b5aae0572daa591d71d7dc1cadeea6c
Giulsmoon <giulia.meneghin1@gmail.com>
2020-12-02 22:42:31 +0100

    modifica al xr interaction manager

---
+++ Assets/ElectricEdgeScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR.Haptics;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ElectricEdgeScript : MonoBehaviour
+{
+    private XRBaseController controller;
+    private XRBaseInteractor interactor;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Electric Edge"))
+        {
+          
+        }
+    }
+
+    public void GetXrController()
+    {
+        XRGrabInteractable grabInteractable;
+    }
+}
