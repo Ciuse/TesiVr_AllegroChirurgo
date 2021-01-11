b56451c7e99c89377c40f04bbb15a1c63e123454
Giulsmoon <giulia.meneghin1@gmail.com>
2021-01-11 18:17:05 +0100

    modifiche al codice pinzare, dobbiamo ancora fixarlo

--- Assets/Scripts/Pinza/pinzare.cs
+++ Assets/Scripts/Pinza/pinzare.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using Vector3 = UnityEngine.Vector3;
 
 public class pinzare : MonoBehaviour
 {
@@ -30,8 +32,8 @@ public class pinzare : MonoBehaviour
     public bool collided;
    
     XRGrabInteractable m_InteractableBase;
+    public bool m_gripDown;
     public bool m_TriggerDown;
-    public bool b_Button;
     public float m_TriggerHeldTime;
     
     public float speed;
@@ -43,7 +45,9 @@ public class pinzare : MonoBehaviour
         m_InteractableBase = GetComponent<XRGrabInteractable>();
         m_InteractableBase.onActivate.AddListener(TriggerPulled);
         m_InteractableBase.onDeactivate.AddListener(TriggerReleased);
-        
+        m_InteractableBase.onSelectEntered.AddListener(GripPulled);
+        m_InteractableBase.onSelectExited.AddListener(GripReleased);
+
         isMoving = false;
         Vector3 localPos1 = pinza1.localPosition;
         startPosPinza1 = new Vector3(localPos1.x, localPos1.y, localPos1.z);
@@ -52,6 +56,11 @@ public class pinzare : MonoBehaviour
 
     }
 
+    void TriggerPulled(XRBaseInteractor obj)
+    {
+        m_TriggerDown = true;
+    }
+    
     void TriggerReleased(XRBaseInteractor obj)
     {
 
@@ -60,183 +69,246 @@ public class pinzare : MonoBehaviour
 
     }
 
-    void TriggerPulled(XRBaseInteractor obj)
+  
+    void GripPulled(XRBaseInteractor obj)
     {
-        m_TriggerDown = true;
+        m_gripDown = true;
     }
+
+    void GripReleased(XRBaseInteractor obj)
+    {
+        m_gripDown = false;
+        m_TriggerDown = false;
+        m_TriggerHeldTime = 0;
+    }
+
 
     public void FixedUpdate()
     {
-        
-        // check collision with tweezer and object
-        if (m_TriggerDown)
+        if (m_gripDown)
         {
-            m_TriggerHeldTime += Time.deltaTime;
 
-            if (m_TriggerHeldTime >= k_HeldThreshold)
+
+            // check collision with tweezer and object
+            if (m_TriggerDown)
             {
-                
-                ClosePinze();
-                
+                m_TriggerHeldTime += Time.deltaTime;
+
+                if (m_TriggerHeldTime >= k_HeldThreshold)
+                {
+
+                    ClosePinze();
+                    if(!collided)
+                    {
+                        CheckCollidersWhileNoObject();
+                    }
+                    else
+                    {
+                        if (Time.frameCount % 20 == 0)
+                        {
+                            CheckCollidersWhileObjectPinzato();
+
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ResetPinze();
+            }       
+        }
+        else{
+          ResetPinze();
+        }
+    }
+
+    private bool IsPinza1Collided()
+    {
+           
+        foreach (GameObject sphere in sphereListPinza1)
+        {
+            var sphereMovableCollisions =
+                Physics.OverlapSphereNonAlloc(sphere.transform.position, collisionRadius, _colliders,
+                    sphereCollisionMask);
+            if (sphereMovableCollisions > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void CheckCollidersWhileObjectPinzato()
+    {
+        if (IsPinza1Collided())
+        {
+            pinza1Collided = true;
+            if (objectWithPinza1 != null)
+            {
+                objectWithPinza1.GetComponent<Collider>().attachedRigidbody.velocity=Vector3.zero;
+                objectWithPinza1.GetComponent<Collider>().attachedRigidbody.angularVelocity = Vector3.zero;
             }
         }
         else
         {
-            ResetPinze();
+            pinza1Collided = false;
+           
         }
 
-/*
-//tastiera input
-if (Keyboard.current.kKey.isPressed)
-{
-    ClosePinze();
-}
-else
-{
-    ResetPinze();
-}  
-
-*/
-
-if (isMoving)
-{
-    if(IsPinza1Collided()){
-        pinza1Collided = true;
-        if(objectWithPinza1==null)
-            objectWithPinza1 = _colliders[0].gameObject;
-    }
-    else
-    {
-        pinza1Collided = false;
-        objectWithPinza1 = null;
-    }
 
 
-   
-    if (IsPinza2Collided())
-    {
-        pinza2Collided = true;
-        if(objectWithPinza2==null) 
-            objectWithPinza2 = _colliders[0].gameObject;
-    }
-    else
-    {
-        pinza2Collided = false;
-        objectWithPinza2 = null;
-    }
-
-    if (pinza1Collided && pinza2Collided)
-    {
-        StopMovement();
-        if( objectWithPinza1 !=null && objectWithPinza2 !=null)
-        { 
-            if (objectWithPinza1 == objectWithPinza2)
+        if (IsPinza2Collided())
+        {
+            pinza2Collided = true;
+            if (objectWithPinza2 != null)
             {
-                objectWithPinza1.transform.SetParent(this.gameObject.transform);
-                objectWithPinza1.GetComponent<Collider>().attachedRigidbody.useGravity = false;
-                objectWithPinza1.GetComponent<Collider>().attachedRigidbody.velocity =Vector3.zero;
-                objectWithPinza1.GetComponent<Collider>().attachedRigidbody.freezeRotation = true;
+                objectWithPinza2.GetComponent<Collider>().attachedRigidbody.velocity=Vector3.zero;
+                objectWithPinza2.GetComponent<Collider>().attachedRigidbody.angularVelocity = Vector3.zero;
+            }
+        }
+        else
+        {
+            pinza2Collided = false;
+            
+        }
 
+        if (!pinza1Collided || !pinza2Collided)
+        {
+            collided = false;
+            if (objectWithPinza1 != null)
+            {
+                objectWithPinza1.transform.SetParent(null);
+                objectWithPinza1.GetComponent<Collider>().attachedRigidbody.useGravity = true;
+                objectWithPinza1 = null;
+            }
+            if (objectWithPinza2 != null)
+            {
+                objectWithPinza2.transform.SetParent(null);
+                objectWithPinza2.GetComponent<Collider>().attachedRigidbody.useGravity = true;
+                objectWithPinza2 = null;
             }
         }
     }
-}
+    
+    
+    public void CheckCollidersWhileNoObject()
+    {
+        if (IsPinza1Collided())
+        {
+            pinza1Collided = true;
+            if (objectWithPinza1 == null)
+                objectWithPinza1 = _colliders[0].gameObject;
+        }
+        else
+        {
+            pinza1Collided = false;
+           
+        }
 
-}
 
-private bool IsPinza1Collided()
-{
+
+        if (IsPinza2Collided())
+        {
+            pinza2Collided = true;
+            if (objectWithPinza2 == null)
+                objectWithPinza2 = _colliders[0].gameObject;
+        }
+        else
+        {
+            pinza2Collided = false;
+            
+        }
+
+        if (pinza1Collided && pinza2Collided)
+        {
+            if (objectWithPinza1 != null && objectWithPinza2 != null)
+            {
+                if (objectWithPinza1 == objectWithPinza2)
+                {
+                    collided = true;
+                    objectWithPinza1.transform.SetParent(this.gameObject.transform);
+                    objectWithPinza1.GetComponent<Collider>().attachedRigidbody.useGravity = false;
+                    objectWithPinza1.GetComponent<Collider>().attachedRigidbody.velocity = Vector3.zero;
+                    objectWithPinza1.GetComponent<Collider>().attachedRigidbody.angularVelocity = Vector3.zero;
+                }
+            }
+        }
+    }
+    private bool IsPinza2Collided()
+    {
+        foreach (GameObject sphere in sphereListPinza2)
+        {
+            var sphereMovableCollisions =
+                Physics.OverlapSphereNonAlloc(sphere.transform.position, collisionRadius, _colliders,
+                    sphereCollisionMask);
+            if (sphereMovableCollisions > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }  
+
+    void ClosePinze()
+    {
+        if (!collided)
+        {
+            isMoving = true;
+            pinza1.localPosition = Vector3.MoveTowards(pinza1.localPosition, endPosPinza1.localPosition, Time.deltaTime * speed);
+            pinza2.localPosition = Vector3.MoveTowards(pinza2.localPosition, endPosPinza2.localPosition, Time.deltaTime * speed);
+        }
+
+
+    }
+
+    void ResetPinze()
+    {
+        //il reset delle pinze viene invocato solo se le due posizioni sono diverse perchè vuol dire che la pinza era stata compressa e deve ritornare
+        //allo stato iniziale 
+        if ((pinza1.localPosition != startPosPinza1) || (pinza2.localPosition != startPosPinza2))
+        {
+            pinza1.localPosition = Vector3.MoveTowards(pinza1.localPosition, startPosPinza1, Time.deltaTime * speed);
+            pinza2.localPosition = Vector3.MoveTowards(pinza2.localPosition, startPosPinza2, Time.deltaTime * speed);
+           
+        }
+
+        if (pinza2Collided || pinza1Collided)
+        {
+            pinza2Collided = false;
+            pinza1Collided = false;
+            if (objectWithPinza1 != null)
+            {
+                objectWithPinza1.transform.SetParent(null);
+                objectWithPinza1.GetComponent<Collider>().attachedRigidbody.useGravity = true;
+                objectWithPinza1 = null;
+            }
+            if (objectWithPinza2 != null)
+            {
+                objectWithPinza2.transform.SetParent(null);
+                objectWithPinza2.GetComponent<Collider>().attachedRigidbody.useGravity = true;
+                objectWithPinza2 = null;
+            }
+        }
+
+        collided = false;
+        isMoving = false;
+    }
+
    
-foreach (GameObject sphere in sphereListPinza1)
-{
-    var sphereMovableCollisions =
-        Physics.OverlapSphereNonAlloc(sphere.transform.position, collisionRadius, _colliders,
-            sphereCollisionMask);
-    if (sphereMovableCollisions > 0)
+    private void OnDrawGizmos()
     {
-        return true;
+        foreach (GameObject sphere in sphereListPinza1)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(sphere.transform.position, collisionRadius);
+
+        }
+
+        foreach (GameObject sphere in sphereListPinza2)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(sphere.transform.position, collisionRadius);
+        }
+
     }
-}
-return false;
-}
-
-private bool IsPinza2Collided()
-{
-foreach (GameObject sphere in sphereListPinza2)
-{
-    var sphereMovableCollisions =
-        Physics.OverlapSphereNonAlloc(sphere.transform.position, collisionRadius, _colliders,
-            sphereCollisionMask);
-    if (sphereMovableCollisions > 0)
-    {
-        return true;
-    }
-}
-return false;
-}  
-
-void StopMovement()
-{
-
-collided = true; 
-isMoving = false;
-
-}
-
-void ClosePinze()
-{
-if (!collided)
-{
-    isMoving = true;
-    pinza1.localPosition = Vector3.MoveTowards(pinza1.localPosition, endPosPinza1.localPosition, Time.deltaTime * speed);
-    pinza2.localPosition = Vector3.MoveTowards(pinza2.localPosition, endPosPinza2.localPosition, Time.deltaTime * speed);
-}
-
-
-}
-
-void ResetPinze()
-{
-//il reset delle pinze viene invocato solo se le due posizioni sono diverse perchè vuol dire che la pinza era stata compressa e deve ritornare
-//allo stato iniziale 
-if ((pinza1.localPosition != startPosPinza1) || (pinza2.localPosition != startPosPinza2))
-{
-    pinza1.localPosition = Vector3.MoveTowards(pinza1.localPosition, startPosPinza1, Time.deltaTime * speed);
-    pinza2.localPosition = Vector3.MoveTowards(pinza2.localPosition, startPosPinza2, Time.deltaTime * speed);
-   
-}
-
-if (pinza2Collided || pinza1Collided)
-{
-    pinza2Collided = false;
-    pinza1Collided = false;
-    if (objectWithPinza1 != null)
-    {
-        objectWithPinza1.transform.SetParent(null);
-        objectWithPinza1.GetComponent<Collider>().attachedRigidbody.useGravity = true;
-    }
-        
-}
-
-collided = false;
-isMoving = false;
-}
-
-
-private void OnDrawGizmos()
-{
-foreach (GameObject sphere in sphereListPinza1)
-{
-    Gizmos.color = Color.green;
-    Gizmos.DrawWireSphere(sphere.transform.position, collisionRadius);
-
-}
-
-foreach (GameObject sphere in sphereListPinza2)
-{
-    Gizmos.color = Color.green;
-    Gizmos.DrawWireSphere(sphere.transform.position, collisionRadius);
-}
-
-}
 }
