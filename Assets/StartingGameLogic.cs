fca4e4f68c5809cd2dddde13e1dd7cbc2fde5472
Giulsmoon <giulia.meneghin1@gmail.com>
2021-02-15 17:48:50 +0100

    foto

---
+++ Assets/StartingGameLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartingGameLogic : MonoBehaviour
+{
+    
+    public List<Sprite> imagesCards = new List<Sprite>();
+    private List<Sprite> imagesCardsSuccess = new List<Sprite>();
+    public Image imageCard;
+    private int numChoosen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //drawCard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void drawCard()
+    {
+        numChoosen = Random.Range(0, imagesCards.Count);
+        imageCard.sprite = imagesCards[numChoosen];
+    }
+}
