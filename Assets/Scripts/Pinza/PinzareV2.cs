2a952e25bcc25d1644197dab1b99ea6c4a0d5c0b
Giulsmoon <giulia.meneghin1@gmail.com>
2021-02-22 18:24:19 +0100

    New pinzaggio e joint

--- Assets/Scripts/Pinza/PinzareV2.cs
+++ Assets/Scripts/Pinza/PinzareV2.cs
@@ -1,77 +1,46 @@
 ﻿using System.Collections.Generic;
 using EventSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 using Vector3 = UnityEngine.Vector3;
 
 public class PinzareV2 : MonoBehaviour
 {
-    public Transform pinza1;
-    public Transform pinza2;
-    public Transform endPosPinza1;
-    public Transform endPosPinza2;
-    public Vector3 startPosPinza1;
-    public Vector3 startPosPinza2;
-    
     public List<GameObject> sphereListPinza1;
     public List<GameObject> sphereListPinza2;
- 
+
+    public Animator animatorPinza1;
+    public Animator animatorPinza2;
+    private float _smoothTriggerValue=0f;
 
     public float collisionRadius;
     public Collider[] _colliders = new Collider[10];
     public LayerMask sphereCollisionMask;
     
-    public List<GameObject> rectListPinza1;
-    public List<GameObject> rectListPinza2;
-    public Vector3 cubeDimension;
-    public LayerMask cubeCollisionMask;
-
     public bool pinza1Collided = false;
     public bool pinza2Collided = false;
     public GameObject objectWithPinza1 = null;
     public GameObject objectWithPinza2 = null;
-    public bool isMoving;
     public bool collided;
    
-    XRGrabInteractable m_InteractableBase;
-    public bool m_gripDown;
-    public bool m_TriggerDown;
-    public float m_TriggerHeldTime;
-    
-    public float speed;
-    const float k_HeldThreshold = 0.1f;
+    public XRGrabInteractable m_InteractableBase;
+    public bool m_gripDown;    
 
-    public GameEvent raiseError;
+    public InputActionReference triggerPressing;
 
     // Start is called before the first frame update
     void Start()
     {
         m_InteractableBase = GetComponent<XRGrabInteractable>();
-        m_InteractableBase.onActivate.AddListener(TriggerPulled);
-        m_InteractableBase.onDeactivate.AddListener(TriggerReleased);
         m_InteractableBase.onSelectEntered.AddListener(GripPulled);
         m_InteractableBase.onSelectExited.AddListener(GripReleased);
 
-        isMoving = false;
-        Vector3 localPos1 = pinza1.localPosition;
-        startPosPinza1 = new Vector3(localPos1.x, localPos1.y, localPos1.z);
-        Vector3 localPos2 = pinza2.localPosition;
-        startPosPinza2 = new Vector3(localPos2.x, localPos2.y, localPos2.z);
+        
 
     }
 
-    void TriggerPulled(XRBaseInteractor obj)
-    {
-        m_TriggerDown = true;
-    }
-    
-    void TriggerReleased(XRBaseInteractor obj)
-    {
 
-        m_TriggerDown = false;
-        m_TriggerHeldTime = 0;
-
-    }
 
   
     void GripPulled(XRBaseInteractor obj)
@@ -82,8 +51,6 @@ public class PinzareV2 : MonoBehaviour
     void GripReleased(XRBaseInteractor obj)
     {
         m_gripDown = false;
-        m_TriggerDown = false;
-        m_TriggerHeldTime = 0;
     }
 
 
@@ -91,26 +58,31 @@ public class PinzareV2 : MonoBehaviour
     {
         if (m_gripDown)
         {
+            animatorPinza1.SetBool("ClosePinza1",true);
+            animatorPinza2.SetBool("ClosePinza2",true);
 
-
+            float triggerValue = triggerPressing.action.ReadValue<float>();
             // check collision with tweezer and object
-            if (m_TriggerDown)
-            {
-                m_TriggerHeldTime += Time.deltaTime;
-
-                if (m_TriggerHeldTime >= k_HeldThreshold)
+            if (triggerValue>0.05f)
+            {  
+                if(!collided)
                 {
-
-                    
-                    if(!collided)
+                    if (_smoothTriggerValue <= triggerValue)
                     {
-                        ClosePinze();
-                        CheckCollidersWhileNoObject();
+                        _smoothTriggerValue = _smoothTriggerValue + 0.02f;
                     }
                     else
                     {
-                        CheckCollidersWhileObjectPinzato();
+                        _smoothTriggerValue = _smoothTriggerValue - 0.02f;
                     }
+                    animatorPinza2.SetFloat("TriggerPressing", _smoothTriggerValue); 
+                    //animatorPinza1.SetFloat("TriggerValue", Random.Range(0f,1f));    
+                    //ClosePinze();
+                    CheckCollidersWhileNoObject();
+                }
+                else
+                {
+                    CheckJoint();
                 }
             }
             else
@@ -119,80 +91,38 @@ public class PinzareV2 : MonoBehaviour
             }       
         }
         else{
-          ResetPinze();
+            ResetPinze();
+
         }
     }
 
-    public void RemoveVelocity()
+
+    public void ClosePinze()
     {
-        if (objectWithPinza1 != null && objectWithPinza2 != null )
-        {
-            print("reset velocity");
-            objectWithPinza1.GetComponent<Collider>().attachedRigidbody.velocity=Vector3.zero;
-            objectWithPinza1.GetComponent<Collider>().attachedRigidbody.angularVelocity = Vector3.zero;
-        }
-
+       
     }
-
     private bool IsPinza1Collided()
     {
            
-        // foreach (GameObject sphere in sphereListPinza1)
-        // {
-        //     var sphereMovableCollisions =
-        //         Physics.OverlapSphereNonAlloc(sphere.transform.position, collisionRadius, _colliders,
-        //             sphereCollisionMask);
-        //     if (sphereMovableCollisions > 0)
-        //     {
-        //         return true;
-        //     }
-        // }
-        // return false;
-
-        foreach (GameObject rect in rectListPinza1)
-            
+        foreach (GameObject sphere in sphereListPinza1)
         {
-            var cubeMovableCollisions =
-                Physics.OverlapBoxNonAlloc(rect.transform.position, cubeDimension / 2, _colliders, Quaternion.identity,
-                    cubeCollisionMask);
-            if (cubeMovableCollisions > 0)
+            var sphereMovableCollisions =
+                Physics.OverlapSphereNonAlloc(sphere.transform.position, collisionRadius, _colliders,
+                    sphereCollisionMask);
+            if (sphereMovableCollisions > 0)
             {
                 return true;
             }
         }
         return false;
+
     }
 
-    public void CheckCollidersWhileObjectPinzato()
+    public void CheckJoint()
     {
-        if (IsPinza1Collided())
+        if (objectWithPinza1.GetComponent<FixedJoint>() == null)
         {
-            pinza1Collided = true;
-        }else
-        {
-            pinza1Collided = false;   
-        }
-
-        if (IsPinza2Collided())
-        {
-            pinza2Collided = true;          
-        }else
-        {
-            pinza2Collided = false;           
-        }
-
-        if (!pinza1Collided || !pinza2Collided)
-        {
-            print("entrato");
-            collided = false;
-            if (objectWithPinza1 != null)
-            {
-              
-            }
-            if (objectWithPinza2 != null)
-            {
-              
-            }
+            ResetPinze();
         }
     }
     
@@ -238,77 +168,53 @@ public class PinzareV2 : MonoBehaviour
                         fixedJoint.connectedBody = gameObject.GetComponent<Rigidbody>();
                         fixedJoint.breakTorque = 200;
                         fixedJoint.breakForce = 200;
+                        fixedJoint.enableCollision = true;
                     }
                    
-
                 }
             }
         }
     }
     private bool IsPinza2Collided()
     {
-        // foreach (GameObject sphere in sphereListPinza2)
-        // {
-        //     var sphereMovableCollisions =
-        //         Physics.OverlapSphereNonAlloc(sphere.transform.position, collisionRadius, _colliders,
-        //             sphereCollisionMask);
-        //     if (sphereMovableCollisions > 0)
-        //     {
-        //         return true;
-        //     }
-        // }
-        // return false;
-        
-        foreach (GameObject rect in rectListPinza2)
-            
+        foreach (GameObject sphere in sphereListPinza2)
         {
-            var cubeMovableCollisions =
-                Physics.OverlapBoxNonAlloc(rect.transform.position, cubeDimension / 2, _colliders, Quaternion.identity,
-                    cubeCollisionMask);
-            if (cubeMovableCollisions > 0)
+            var sphereMovableCollisions =
+                Physics.OverlapSphereNonAlloc(sphere.transform.position, collisionRadius, _colliders,
+                    sphereCollisionMask);
+            if (sphereMovableCollisions > 0)
             {
                 return true;
             }
         }
         return false;
+        
+  
     }  
-
-    void ClosePinze()
-    {
-    
-            isMoving = true;
-            pinza1.localPosition = Vector3.MoveTowards(pinza1.localPosition, endPosPinza1.localPosition, Time.deltaTime * speed);
-            pinza2.localPosition = Vector3.MoveTowards(pinza2.localPosition, endPosPinza2.localPosition, Time.deltaTime * speed);
-
-    }
 
     void ResetPinze()
     {
         //il reset delle pinze viene invocato solo se le due posizioni sono diverse perchè vuol dire che la pinza era stata compressa e deve ritornare
         //allo stato iniziale 
-        if ((pinza1.localPosition != startPosPinza1) || (pinza2.localPosition != startPosPinza2))
+        
+        animatorPinza1.SetBool("ClosePinza1",false);
+        animatorPinza2.SetBool("ClosePinza2",false);
+        
+        if (objectWithPinza1 != null)
         {
-            pinza1.localPosition = Vector3.MoveTowards(pinza1.localPosition, startPosPinza1, Time.deltaTime * speed);
-            pinza2.localPosition = Vector3.MoveTowards(pinza2.localPosition, startPosPinza2, Time.deltaTime * speed);
-           
-        }
-
-        if (pinza2Collided || pinza1Collided)
-        {
-            pinza2Collided = false;
-            pinza1Collided = false;
-            if (objectWithPinza1 != null)
+            if (objectWithPinza1.GetComponent<FixedJoint>() != null)
             {
-               
-            }
-            if (objectWithPinza2 != null)
-            {
-               
-            }
+                objectWithPinza1.GetComponent<FixedJoint>().breakForce = 0f;
+            } 
         }
+     
 
+        pinza2Collided = false;
+        pinza1Collided = false;
+        objectWithPinza1 = null;
+        objectWithPinza2 = null;
         collided = false;
-        isMoving = false;
+        _smoothTriggerValue = 0f;
     }
 
    
@@ -325,18 +231,6 @@ public class PinzareV2 : MonoBehaviour
         {
             Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(sphere.transform.position, collisionRadius);
-        }
-
-        foreach (GameObject cube in rectListPinza1)
-        {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawWireCube(cube.transform.position, cubeDimension);
-        }
-        
-        foreach (GameObject cube in rectListPinza2)
-        {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawWireCube(cube.transform.position, cubeDimension);
         }
 
     }
--- Assets/consoleCanvas.cs
+++ Assets/consoleCanvas.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class consoleCanvas : MonoBehaviour
 {
     public  GameObject canvas;
 
     private TextMeshProUGUI textArea;
+    public InputActionReference triggerPressing;
+    [SerializeField]
+    private int maxLines = 15;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +20,9 @@ public class consoleCanvas : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {     
+        ClearLines();
+        textArea.text = triggerPressing.action.ReadValue<float>().ToString();
     }
 
     public void AddErrore()
@@ -25,5 +30,11 @@ public class consoleCanvas : MonoBehaviour
         textArea.text = textArea.text + "\n OGGETTO CADUTO";
     }
     
-    
+    private void ClearLines()
+    {
+        if (textArea.text.Split('\n').Length >= maxLines)
+        {
+            textArea.text = string.Empty;
+        }
+    }
 }
