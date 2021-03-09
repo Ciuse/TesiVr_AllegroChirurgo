7f97b846b284ec7eaafe72ff67b3d9561651087f
Giulsmoon <giulia.meneghin1@gmail.com>
2021-03-09 18:38:26 +0100

    creazione scena iniziale con settaggio mani, aggiunta script per pinzare con la sinistra

---
+++ Assets/SceneLoaderScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EventSystem;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour
+{
+
+    
+
+    public void LoadSceneRightHandGrab()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void LoadSceneLeftHandGrab()
+    {
+       
+        SceneManager.LoadScene(2);
+        
+    }
+    
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+    
+}
