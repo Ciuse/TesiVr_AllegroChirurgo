9b45af35e40d8e162ac8b25c689d5ea72f5c71e4
Ciuse <giuseppe3.mauri@mail.polimi.it>
2021-03-16 11:27:45 +0100

    Nuget, firebase REST added, removed firebase sdk

---
+++ Assets/FireBaseProvaREST.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Firebase.Database;
+
+public class FireBaseProvaREST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+//        var firebase = new FirebaseClient("https://allegrochirurgovr.firebaseio.com");
+//
+//        await firebase
+//            .Child("prova").PostAsync("a");
+//        await firebase
+//            .Child("prova").PutAsync("a");
+
+        var firebase2 = new FirebaseClient("https://allegrochirurgovr-default-rtdb.europe-west1.firebasedatabase.app/");
+
+
+        await firebase2
+            .Child("SONOILVR2").PutAsync("{\"appeared\": -70000000}");
+}
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
