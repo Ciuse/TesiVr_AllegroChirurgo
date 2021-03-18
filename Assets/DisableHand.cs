9ba47c67a6f86f8f079f3f857dbed53db195c2f5
Giulsmoon <giulia.meneghin1@gmail.com>
2021-03-18 17:10:28 +0100

    settings UI manage with events

---
+++ Assets/DisableHand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EventSystem;
+using UnityEngine;
+
+public class DisableHand : MonoBehaviour
+{
+    public GameEvent disableHandEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void DisableHandAfterButtonClick()
+    {
+        disableHandEvent.Raise();
+    }
+}
