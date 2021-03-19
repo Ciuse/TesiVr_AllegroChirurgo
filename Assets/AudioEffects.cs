4aa7b97c9d7fc10f399b8739b85ad831fcdd5453
Giulsmoon <giulia.meneghin1@gmail.com>
2021-03-19 21:37:31 +0100

    ui sound

---
+++ Assets/AudioEffects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EventSystem;
+using UnityEngine;
+
+public class AudioEffects : MonoBehaviour
+{
+    public GameEvent audioEffectsEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateAudioEffects()
+    {
+        audioEffectsEvent.Raise();
+    }
+}
---
+++ Assets/UI_SFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_SFX : MonoBehaviour
+{
+    
+    public AudioSource UIAudioSource;
+    
+    public AudioClip menuClickClip;
+    
+    public void MenuClickAudio()
+    {
+        var randPitch = Random.Range(0.9f, 1.1f);
+        UIAudioSource.pitch = randPitch;
+        UIAudioSource.PlayOneShot(menuClickClip);    
+    }
+}
