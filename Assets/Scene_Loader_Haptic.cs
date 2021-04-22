6a4080a10d442eb4027001c4e8f8b1f816c09540
Giulsmoon <giulia.meneghin1@gmail.com>
2021-04-22 21:45:16 +0200

    adding starting scene haptic

---
+++ Assets/Scene_Loader_Haptic.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Loader_Haptic : MonoBehaviour
+{
+    public bool vibrationSetting;
+    public bool hideHandSetting;
+    public bool visualPinzaSetting;
+    public bool visualObjectSetting;
+    public bool soundObjectSetting;
+    public bool detectObjectCollision;
+    public bool detectPinzaCollision;
+
+    public void Start()
+    {
+       
+        DontDestroyOnLoad(this);
+    }
+
+
+    public void LoadSceneOnlyHaptic()
+    {
+        SceneManager.LoadScene(4);
+    }
+    
+    
+    
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+
+    public void Vibration()
+    {
+        vibrationSetting = !vibrationSetting;
+    }
+    
+    public void HideHand()
+    {
+        hideHandSetting = !hideHandSetting;
+    }
+    
+    public void VisualPinza()
+    {
+        visualPinzaSetting = !visualPinzaSetting;
+    }
+    
+    public void VisualObject()
+    {
+        visualObjectSetting = !visualObjectSetting;
+    }
+    
+    public void SoundObject()
+    {
+        soundObjectSetting = !soundObjectSetting;
+    }
+    
+    public void DetectObjectCollision()
+    {
+        detectObjectCollision = !detectObjectCollision;
+    }
+    
+    public void DetectPinzaCollision()
+    {
+        detectPinzaCollision = !detectPinzaCollision;
+    }
+}
---
+++ Assets/Setting_Manager_Haptic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setting_Manager_Haptic : MonoBehaviour
+{
+    public Scene_Loader_Haptic sceneLoaderHaptic;
+    void Start()
+    {
+        sceneLoaderHaptic = GameObject.Find ("SceneLoader_Haptic").GetComponent<Scene_Loader_Haptic>();
+        print("Visual effect object: "+ sceneLoaderHaptic.visualObjectSetting);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
