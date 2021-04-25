4e3414725b6b5e6395d03ee9c489e382becec096
Giulsmoon <giulia.meneghin1@gmail.com>
2021-04-26 00:05:45 +0200

    naso rosso

---
+++ Assets/LightOnandOff.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOnandOff : MonoBehaviour
+{
+ 
+
+    public GameObject nosePatient;
+
+    private Color colorDefault;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colorDefault = nosePatient.GetComponent<MeshRenderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnLight()
+    {
+        nosePatient.GetComponent<MeshRenderer>().material.color=Color.red;
+    }
+    
+    public void OffLight()
+    {
+        nosePatient.GetComponent<MeshRenderer>().material.color=colorDefault;
+    }
+
+    
+}
