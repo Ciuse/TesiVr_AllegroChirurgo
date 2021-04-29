420e255c705197ba239040b67dc61ce1bb860e85
Giulsmoon <giulia.meneghin1@gmail.com>
2021-04-29 15:59:13 +0200

    fix haptic training

--- Assets/Training_Haptic.cs
+++ Assets/Training_Haptic.cs
@@ -6,29 +6,13 @@ using UnityEngine;
 public class Training_Haptic : MonoBehaviour
 {
     private bool buttonStatus = false;
-    private  GameObject hapticDevice = null;
+    public  GameObject hapticDevice = null;
     public int buttonID = 0;
 
     public GameEvent startingTrainingVocal;
 
     public bool startingVocal = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (hapticDevice == null)
-        {
-
-            HapticPlugin[] HPs = (HapticPlugin[])Object.FindObjectsOfType(typeof(HapticPlugin));
-            foreach (HapticPlugin HP in HPs)
-            {
-                if (HP.hapticManipulator == this.gameObject)
-                {
-                    hapticDevice = HP.gameObject;
-                }
-            }
-
-        }
-    }
+    
 
     // Update is called once per frame
     void Update()
